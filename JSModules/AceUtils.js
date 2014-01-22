5b255e32e8e24244c8862b899be77a7c8474ce2e
paul <paul@paul-OptiPlex-380.(none)>
2014-01-22 10:27:02 +0800

    dddr

--- JSModules/AceUtils.js
+++ JSModules/AceUtils.js
@@ -1,11 +1,12 @@
 define(function(require) {
-
+	//function binding
 	function bind(fn, context) {
 		return function() {
 			fn.apply(context, arguments);
 		}
 	}
 
+	//function curring
 	function curry(fn) {
 		var outterArgs = Array.prototype.slice.call(arguments, 1);
 		return function() {
@@ -22,5 +23,23 @@ define(function(require) {
 			var finalArgs = outterArgs.contact(innerArgs);
 			return fn.apply(context, finalArgs);
 		}
+	}
+
+	//function throttle
+	function throttle(fn, context) {
+		clearTimeout(fn.tId);
+		fn.tId = setTimeout(function () {
+			fn.call(context);
+		});
+	}
+
+	function formatMsg(text, data) {
+		text += '';
+		if (data) {
+			$.each(data, function(key, val) {
+				text = text.replace(new RegExp('\\{\\{' + key + '\\}\\}', 'g'), val);
+			});
+		}
+		return text;
 	}
 });
